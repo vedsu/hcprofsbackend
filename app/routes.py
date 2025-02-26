c80b1f405c64dd72a1f94cfc27cb25a26cc40a6f
vedsu <90701239+vedsu@users.noreply.github.com>
2025-02-26 14:17:06 +0530

    Update routes.py

--- app/routes.py
+++ app/routes.py
@@ -725,13 +725,13 @@ def newsletter_order():
             customeremail = request.form.get('customeremail')
             paymentstatus = request.form.get("paymentstatus")
             newsletter = request.form.get("topic")
-            try:
-                price = mongo.db.newsletter_data.find_one({"topic": newsletter}, {"price": 1, "_id": 0})
-                # Extract the 'price' value and convert to int
-                price_value = int(price["price"]) if price and "price" in price else 0
-                discount = price_value
-            except:
-                discount = 0
+            # try:
+            #     price = mongo.db.newsletter_data.find_one({"topic": newsletter}, {"price": 1, "_id": 0})
+            #     # Extract the 'price' value and convert to int
+            #     price_value = int(price["price"]) if price and "price" in price else 0
+            #     discount = price_value
+            # except:
+            #     discount = 0
                 
             orderamount =  request.form.get("orderamount")
             
